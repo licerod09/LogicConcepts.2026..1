8c06d7cfb01dbc700c7d4d3af5c6e4611d5adbeb
LICETH CAROLINA RODRIGUEZ MEDRANO <licethrodriguez1161710@correo.itm.edu.co>
2026-02-18 23:10:32 -0500

    IsNumberOddOrNott

---
+++ LogicConcepts/IsNumberOddOrNott/Program.cs
@@ -0,0 +1,36 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese número o presione 'Salir' para salir de la ejecución:");
+    numberString = Console.ReadLine();
+
+
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+
+    
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt} es Impar.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"El valor que digitaste {numberString}, no es un número entero.");
+    }
+
+
+
+
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Perfecto");
