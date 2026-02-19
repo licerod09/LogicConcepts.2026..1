096df6dda884831306b6e34960d0c88e1a70f88b
LICETH CAROLINA RODRIGUEZ MEDRANO <licethrodriguez1161710@correo.itm.edu.co>
2026-02-19 14:00:52 -0500

    MultipleOfAnother

---
+++ LogicConcepts/MultipleOfAnother/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es multiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no  es multiplo de {b}");
+
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Perfecto");
